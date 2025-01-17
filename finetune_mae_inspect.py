464aa8a07d465bed28bd4085d387f6cb6c9433ef
peterhan91 <petertianyuhan@hotmail.com>
2025-01-17 11:53:35 +0100

    minor for position interpo

--- finetune_mae_inspect.py
+++ finetune_mae_inspect.py
@@ -242,7 +242,8 @@ def main(args):
                 del checkpoint_model[k]
 
         # interpolate position embedding
-        # interpolate_pos_embed(model, checkpoint_model, old_size=args.old_input_size)
+        if args.old_input_size != args.input_size:
+            interpolate_pos_embed(model, checkpoint_model, old_size=args.old_input_size)
 
         # load pre-trained model
         msg = model.load_state_dict(checkpoint_model, strict=False)
