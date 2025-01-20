d5f992baf068dd7c9ad9368d9d12bc1705b812cd
Tianyu Han <petertianyuhan@hotmail.com>
2025-01-20 22:19:50 +0100

    revert changes

---
+++ other_models/merlin/build.py
@@ -0,0 +1,79 @@
+import copy
+
+import torch
+from torch import nn
+from transformers import AutoModel, AutoTokenizer
+# from nltk.tokenize import wordpunct_tokenize
+import torchvision
+
+from other_models.merlin import i3res
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self, is_marlin=True):
+        super().__init__()
+        if is_marlin:
+            resnet = torchvision.models.resnet152(pretrained=True)
+        else:
+            resnet = torchvision.models.resnet50(pretrained=True)
+        self.i3_resnet = i3res.I3ResNet(
+            copy.deepcopy(resnet), class_nb=1692, conv_class=True
+        )
+
+    def forward(self, image):
+        contrastive_features, ehr_features = self.i3_resnet(image)
+        return contrastive_features, ehr_features
+
+
+class TextEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained("yikuan8/Clinical-Longformer")
+        self.text_encoder = AutoModel.from_pretrained("yikuan8/Clinical-Longformer")
+        self.text_encoder.gradient_checkpointing_enable()
+        self.linear_layer = nn.Linear(768, 512)
+
+    def forward(self, text_labels):
+        text_labels = [sanitize_report(text) for text in text_labels]
+        inputs = self.tokenizer(
+            text_labels,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=1024,
+        )
+        inputs = {k: v.to(self.text_encoder.device) for k, v in inputs.items()}
+        text_embeddings = self.text_encoder(**inputs).last_hidden_state[:, 0, :]
+        text_embeddings = self.linear_layer(text_embeddings)
+        return text_embeddings
+
+
+class MerlinArchitecture(nn.Module):
+    def __init__(self, init_logit_scale: float = 1.0):
+        super().__init__()
+        self.encode_image = ImageEncoder()
+        self.encode_text = TextEncoder()
+        self.logit_scale = nn.Parameter(torch.ones([]) * init_logit_scale)
+
+    def forward(self, image, text):
+        image_features, ehr_features = self.encode_image(image)
+        text_features = self.encode_text(text)
+
+        if len(image_features.shape) == 1:
+            image_features = image_features.unsqueeze(0)
+        if len(text_features.shape) == 1:
+            text_features = text_features.unsqueeze(0)
+
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        return (
+            image_features,
+            ehr_features,
+            text_features,
+        )
+
+
+def sanitize_report(report):
+    report = report.lower()
+    return " ".join(wordpunct_tokenize(report))
---
+++ other_models/merlin/load.py
@@ -0,0 +1,37 @@
+import os
+
+from huggingface_hub import hf_hub_download
+import torch
+from torch import nn
+
+from other_models.merlin import build, utils
+
+
+class Merlin(nn.Module):
+    def __init__(self):
+        super(Merlin, self).__init__()
+        self.current_path = os.path.dirname(os.path.abspath(__file__))
+        self.local_dir = os.path.join(self.current_path, "checkpoints")
+        self.checkpoint_name = (
+            "i3_resnet_clinical_longformer_best_clip_04-02-2024_23-21-36_epoch_99.pt"
+        )
+        self.repo_id = "louisblankemeier/Merlin"
+        self.model = self._load_model()
+
+    def _load_model(self):
+        self._download_checkpoint()
+        model = build.MerlinArchitecture()
+        model.load_state_dict(
+            torch.load(os.path.join(self.local_dir, self.checkpoint_name))
+        )
+        return model
+
+    def _download_checkpoint(self):
+        utils.download_file(
+            repo_id=self.repo_id,
+            filename=self.checkpoint_name,
+            local_dir=self.local_dir,
+        )
+
+    def forward(self, *input):
+        return self.model(*input)
